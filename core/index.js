f96c4daa8ccc2fd11ac87bcb3347665d57f94365
caijiadi <459953103@qq.com>
2021-07-29 16:34:25 +0800

    feat: 基础框架搭好

--- core/index.js
+++ core/index.js
@@ -5,10 +5,19 @@ const chalk = require('chalk')
 const { program } = require('commander')
 program.version('0.0.1-alpha')
 
-program.option('-v, --version', 'output tool version')
+// 配置对应命令参数
+program
+  .option('-v, --version', 'output tool version')
+  .option('-e, --entry', 'custom entry')
+  .option('-o, --output', 'output dirtory')
+
 program.parse(process.argv)
 
 const options = program.opts()
 if (options.version) {
   console.log(chalk.green(package.version))
 }
+
+const entry_file = program.entry || 'src'
+const output = program.output || 'src'
+
